03bb3c72b2d6d289b69ae54b8190dd3c9f7aa93c
Enrique Montemayor <eemontemayor@live.com>
2019-03-19 12:55:22 -0500

    m

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import STORE from './STORE.js';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
+  ReactDOM.render(
+    <App store={STORE} />,
+    div,
+  );
   ReactDOM.unmountComponentAtNode(div);
+ 
 });
